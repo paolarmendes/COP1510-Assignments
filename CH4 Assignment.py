13da91b083832efb3293684374567366ebcb550c
Paola Ramos Mendes <127321747+paolarmendes@users.noreply.github.com>
2026-04-21 10:43:48 -0500

    CH4 Assignment

--- CH4 Assignment.py
+++ CH4 Assignment.py
@@ -1,6 +1,6 @@
-#Paola Ramos Mendes
-#CH4 Assignment 
-#02/20/2026
+#Write a program that asks the user for the speed of a vehicle (in miles per hours) and the number of hours it has traveled. The program should then use a loop to display the distance the vehicle has traveled for each our of that time period.
+#Validate both inputs when they entered so your program will not accept a value less than 1 for speed or hours traveled. 
+#02/21/2026
 speed=int(input("What is the speed of the vehicle in mph? ")) #Here I asked the user for the speed of a vehicle in miles per hour
 while speed < 1: #confirms that speed must be at least 1
     print("Speed must be 1 or greater") #if its less than one, this will be printed 
