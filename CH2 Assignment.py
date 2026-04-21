2f574b0f1843ee89965b25c2b1cc4766ad4701ce
Paola Ramos Mendes <127321747+paolarmendes@users.noreply.github.com>
2026-04-21 10:39:32 -0500

    CH2 Assignment

--- CH2 Assignment.py
+++ CH2 Assignment.py
@@ -1,5 +1,6 @@
 #Your programming assignment for chapter 2 is to create a simple Python program that will calculate the tax, tip, and total of a restaurant bill. The primary purpose of this program is to make sure you have your Python environment setup and you can create a basic program.
 #The program should ask the user to input the cost of the meal. Then, you will calculate the tax as 7% of the meal charge. Next, calculate the tip as 20% of the meal charge. Finally, find the total for the meal by adding the meal charge, tax, and tip. Display the output, including the amount for the tax, tip, and total.
+#01/31/2026
 mealCharge = float(input("Enter the cost of the meal: "))
 
 taxAmt = mealCharge * .07
